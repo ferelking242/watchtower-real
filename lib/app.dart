9b523cb6bf3a961e05310860d463d730a5a5ac7d
FERELKING242 <111065891+ferelking242@users.noreply.github.com>
2026-07-18 19:18:22 +0100

    fix: rename WatchtowerRealApp→ReelApp, fix package import reel, use router directly

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,14 @@
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:watchtower_real/router/router.dart';
+import 'package:reel/router/router.dart';
 
-class WatchtowerRealApp extends ConsumerWidget {
-  const WatchtowerRealApp({super.key});
+class ReelApp extends StatelessWidget {
+  const ReelApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final router = ref.watch(routerProvider);
+  Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Watchtower Real',
+      title: 'Reel',
       debugShowCheckedModeBanner: false,
       theme: FlexThemeData.light(scheme: FlexScheme.deepBlue),
       darkTheme: FlexThemeData.dark(scheme: FlexScheme.deepBlue),
